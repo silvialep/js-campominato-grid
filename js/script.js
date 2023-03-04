7a0df3105c76353e417331bfb9654eea39c3ed5a
silvialep <silvia.lepore_sl@libero.it>
2023-03-04 12:05:03 +0100

    feat: created function to generate grid

--- js/script.js
+++ js/script.js
@@ -3,8 +3,8 @@ Consegna
 L'utente clicca su un bottone che genererà una griglia di gioco quadrata.
 Ogni cella ha un numero progressivo, da 1 a 100.
 Ci saranno quindi 10 caselle per ognuna delle 10 righe.
-Quando l'utente clicca su ogni cella, la cella cliccata si colora di azzurro 
-ed emetto un messaggio in console con il numero della cella cliccata.
+Quando l'utente clicca su ogni cella, la cella cliccata si colora di azzurro ed emetto 
+un messaggio in console con il numero della cella cliccata.
 
 Bonus
 Aggiungere una select accanto al bottone di generazione, 
@@ -20,6 +20,26 @@ Scriviamo prima cosa vogliamo fare passo passo in italiano, dividiamo
 il lavoro in micro problemi.
 Ad esempio:
 Di cosa ho bisogno per generare i numeri?
-Proviamo sempre prima con dei console.log() per capire se stiamo ricevendo i dati giusti.
-Le validazioni e i controlli possiamo farli anche in un secondo momento.
+Proviamo sempre prima con dei console.log() per capire se 
+stiamo ricevendo i dati giusti.
+Le validazioni e i controlli possiamo farli anche in un 
+secondo momento.
 */
+
+// creo variabile degli elementi HTML che andrò a manipolare
+let gridContainer = document.getElementById('grid-container');
+let buttonEl = document.getElementById('btn-create');
+
+// creo funzione per generare le 100 celle
+function generateGrid(grid) {
+    for(i = 0; i < 100; i++) {
+        let newCell = document.createElement('div');
+        grid.append(newCell);
+        newCell.innerText = i;
+        newCell.classList.add('cell');
+    }
+}
+
+buttonEl.addEventListener('click', function() {
+    generateGrid(gridContainer);
+})
