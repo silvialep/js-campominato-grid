bbb2f11fd9c8363ebd1642f03217815a4344c6b9
silvialep <silvia.lepore_sl@libero.it>
2023-03-05 14:26:32 +0100

    feat: added h2 in page

--- bonus/js/script.js
+++ bonus/js/script.js
@@ -1,25 +1,18 @@
-/*
-Bonus
-Aggiungere una select accanto al bottone di generazione, 
-che fornisca una scelta tra tre diversi livelli di difficoltà:
-- con difficoltà 1 => 100 caselle, con un numero compreso tra 1 e 100, 
-    divise in 10 caselle per 10 righe;
-- con difficoltà 2 => 81 caselle, con un numero compreso tra 1 e 81, 
-    divise in 9 caselle per 9 righe;
-- con difficoltà 3 => 49 caselle, con un numero compreso tra 1 e 49, 
-    divise in 7 caselle per 7 righe;
-*/
+
 
 // creo variabile degli elementi HTML che andrò a manipolare
 let gridEl = document.getElementById('grid');
 let buttonCreate = document.getElementById('btn-create');
 let buttonClear = document.getElementById('btn-clear');
 
+let gridPlaceholder = document.getElementById('grid-placeholder');
+
 // console.log(difficultyChoice.value);
 gridEl.style.display = 'none';
 
 // creo evento al click per generare la griglia con le celle
 buttonCreate.addEventListener('click', function () {
+    gridPlaceholder.style.display = 'none';
     gridEl.style.display = 'flex';
     let difficultyChoice = document.getElementById('difficulty');
     if (difficultyChoice.value == 'easy') {
